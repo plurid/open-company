0e8dd3fb3567107521e3ee652e48675aea289702
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2023-11-15 22:36:46 +0200

    open-company > dev: routes/Database

--- packages/open-company/src/routes/Database.tsx
+++ packages/open-company/src/routes/Database.tsx
@@ -1,7 +1,7 @@
 import { invoke } from '@tauri-apps/api/tauri';
 import { open } from '@tauri-apps/api/dialog';
 import { createSignal } from 'solid-js';
-import { A } from '@solidjs/router';
+import { A, useNavigate } from '@solidjs/router';
 
 import './Database.css';
 
@@ -9,6 +9,29 @@ import './Database.css';
 
 function Database() {
     const [newDatabase, setNewDatabase] = createSignal('');
+
+    const navigate = useNavigate();
+
+
+    const generateNewDatabase = async () => {
+        if (!newDatabase()) {
+            return;
+        }
+
+        const directory = await open({
+            directory: true,
+        });
+        if (typeof directory !== 'string') {
+            return;
+        }
+
+        await invoke('start_database', {
+            name: newDatabase(),
+            location: directory + `/${newDatabase()}.sqlite`,
+        });
+
+        navigate('/');
+    }
 
 
     return (
@@ -20,29 +43,24 @@ function Database() {
                     placeholder="database name"
                     required
                     value={newDatabase()}
-                    onInput={(e) => {
-                        setNewDatabase(
-                            e.currentTarget.value
-                                .replace(/\s+/g, '')
-                                .trim()
-                        );
+                    onInput={(event) => {
+                        const value = event.currentTarget.value
+                            .replace(/\s+/g, '')
+                            .trim();
+
+                        setNewDatabase(value);
                     }}
+                    spellcheck={false}
+                    autocapitalize="off"
+                    autocorrect="off"
+                    autocomplete="false"
                 />
 
                 <button
-                    onClick={async () => {
-                        if (!newDatabase()) {
-                            return;
-                        }
-
-                        const directory = await open({
-                            directory: true,
-                        });
-
-                        invoke('start_database', {
-                            name: newDatabase(),
-                            location: directory + `/${newDatabase()}.sqlite`,
-                        });
+                    class="disabled:opacity-40 disabled:border-transparent disabled:pointer-events-none"
+                    disabled={!newDatabase()}
+                    onClick={() => {
+                        generateNewDatabase();
                     }}
                 >
                     Generate Database
