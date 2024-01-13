633b7aaab2ef12b0ffdccd66b96e0c0f0c5a1d96
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2024-01-13 18:11:23 +0200

    interclients/specific/xfactura-ro > dev: Extractors

--- packages/interclients/specific/xfactura-ro/containers/Extractors/index.tsx
+++ packages/interclients/specific/xfactura-ro/containers/Extractors/index.tsx
@@ -53,7 +53,7 @@ export default function Extractors({
 
     return (
         <div
-            className="grid gap-2 justify-center items-center text-center min-h-[50px] md:flex md:gap-6"
+            className="grid gap-4 justify-center items-center text-center min-h-[50px] md:flex md:gap-6"
             style={{
                 justifyItems: 'center',
             }}
@@ -79,10 +79,25 @@ export default function Extractors({
                         </>
                     )}
                 >
-                    <LinkButton
-                        text="încărcare"
-                        onClick={() => triggerReadInput()}
-                    />
+                    <div
+                        className="flex items-center gap-1"
+                    >
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 24 24"
+                            style={{
+                                width: '25px',
+                                height: '25px',
+                            }}
+                        >
+                            <path d="M3 19h18v2H3v-2zM13 5.828V17h-2V5.828L4.929 11.9l-1.414-1.414L12 2l8.485 8.485-1.414 1.414L13 5.83z" fill="white" />
+                        </svg>
+
+                        <LinkButton
+                            text="încărcare"
+                            onClick={() => triggerReadInput()}
+                        />
+                    </div>
                 </Tooltip>
             </div>
 
@@ -100,12 +115,27 @@ export default function Extractors({
                                 </>
                             )}
                         >
-                            <LinkButton
-                                text="fotografiere"
-                                onClick={() => {
-                                    setShowCamera(true);
-                                }}
-                            />
+                            <div
+                                className="flex items-center gap-1"
+                            >
+                                <svg xmlns="http://www.w3.org/2000/svg"
+                                    viewBox="0 0 100 100"
+                                    style={{
+                                        width: '25px',
+                                        height: '25px',
+                                    }}
+                                >
+                                    <rect x="10" y="20" width="80" height="60" stroke="white" strokeWidth={8} />
+                                    <circle cx="50" cy="50" r="15" stroke="white" strokeWidth={8} />
+                                </svg>
+
+                                <LinkButton
+                                    text="fotografiere"
+                                    onClick={() => {
+                                        setShowCamera(true);
+                                    }}
+                                />
+                            </div>
                         </Tooltip>
                     </div>
 
@@ -121,10 +151,26 @@ export default function Extractors({
                                 </>
                             )}
                         >
-                            <LinkButton
-                                text="înregistrare"
-                                onClick={() => setShowMicrophone(show => !show)}
-                            />
+                            <div
+                                className="flex items-center gap-1"
+                            >
+                                <svg
+                                    viewBox="0 0 48 48"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    style={{
+                                        width: '25px',
+                                        height: '25px',
+                                    }}
+                                >
+                                    <path d="M24,33a8,8,0,0,0,8-8V9A8,8,0,0,0,16,9V25A8,8,0,0,0,24,33ZM20,9a4,4,0,0,1,8,0V25a4,4,0,0,1-8,0Z" fill="white" />
+                                    <path d="M38,25a2,2,0,0,0-4,0,10,10,0,0,1-20,0,2,2,0,0,0-4,0A14,14,0,0,0,22,38.84V43H21a2,2,0,0,0,0,4h6a2,2,0,0,0,0-4H26V38.84A14,14,0,0,0,38,25Z" fill="white" />
+                                </svg>
+
+                                <LinkButton
+                                    text="înregistrare"
+                                    onClick={() => setShowMicrophone(show => !show)}
+                                />
+                            </div>
                         </Tooltip>
                     </div>
                 </>
