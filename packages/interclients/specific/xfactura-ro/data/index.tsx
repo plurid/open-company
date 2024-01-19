350adae460b8081ca0c799ddbbe0214f8e7e6a71
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2024-01-19 18:14:25 +0200

    interclients/specific/xfactura-ro > dev: data

--- packages/interclients/specific/xfactura-ro/data/index.tsx
+++ packages/interclients/specific/xfactura-ro/data/index.tsx
@@ -118,6 +118,7 @@ export const ENVIRONMENT = {
     IN_PRODUCTION: process.env.NEXT_PUBLIC_IN_PRODUCTION || '',
     X_DOMAIN: process.env.NEXT_PUBLIC_X_DOMAIN || '',
     AI_DOMAIN: process.env.NEXT_PUBLIC_AI_DOMAIN || '',
+    API_DOMAIN: process.env.NEXT_PUBLIC_API_DOMAIN || '',
     GOOGLE_LOGIN: process.env.NEXT_PUBLIC_GOOGLE_LOGIN || '',
 };
 
